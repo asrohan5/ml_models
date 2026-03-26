c8672c1ca526938b247be048ddba4e36fb171347
asrohan5 <rohanrohu99@gmail.com>
2026-03-26 23:17:38 +0530

    Load and Understand

--- classification/classification.py
+++ classification/classification.py
@@ -50,3 +50,65 @@ df = df.rename(columns={'class':'hazard'})
 df['hazard'] = df['hazard'].astype(int)
 
 #print(df.head())
+
+counts = df['hazard'].value_counts()
+
+overview_df = pd.DataFrame({
+    'shape': df.shape,
+    'missing_values': df.isnull().sum().sum(),
+    'safe_shifts': counts[0],
+    'safe_shift_pct': (counts[0]/len(df))*100,
+    'hazard_shifts': counts[1],
+    'hazard_shifts_pct': (counts[1]/len(df))*100,
+    'imbalance_ratio': counts[0]/counts[1]
+
+})
+
+#print(overview_df)
+
+X = df.drop('hazard', axis=1)
+y = df['hazard']
+
+CATEGORICAL = [col for col in X.columns if X[col].dtype == 'object']
+NUMERICAL = [col for col in X.columns if X[col].dtype in ['int64', 'float64']]
+
+# print('Categorical feature values: ')
+# for col in CATEGORICAL:
+#     print(f' {col}: {sorted(df[col].unique())}')
+
+# print('Energy features - scale ranges: ')
+# for col in ['genergy', 'energy', 'maxenergy']:
+#     print(f' {col}: {df[col].min():.0f} to {df[col].max():,.0f}')
+# print()
+
+# print('Energy spans 10^4 to 10^8 - 10000x range')
+# print('This will dominate gradient-based models without scaling')
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y, random_state=42)
+
+# print('Stratified Train/Test Split')
+# print(f'Train: {len(X_train)} rows | positive rate: {y_train.mean()*100:.1f}%')
+# print(f'Test: {len(X_test)} rows | positive rate: {y_test.mean()*100:.1f}%')
+# print()
+
+'''
+Without stratify=y, random chanve could put all 170 hazardous shifts.
+In training with none in test - making evaluation impossible.
+'''
+
+
+#BUILDING PREPROCESSING PIPELINE
+preprocessor = ColumnTransformer([
+    ('num', StandardScaler(), NUMERICAL),
+    ('cat', OneHotEncoder(handle_unknown='ignore', sparse_output=False), CATEGORICAL),
+], remainder='drop')
+
+X_train_prep = preprocessor.fit_transform(X_train)
+X_test_prep = preprocessor.transform(X_test)
+print(f'\n Feature Matrix shape after preprocessing: {X_train_prep.shape}')
+
+
+
